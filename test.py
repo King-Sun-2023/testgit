94df784539351d7c718a724a5c9a8129c0f8047e
King <liepke.iam@Kings-MacBook-Pro.local>
2023-04-04 14:39:52 -0700

    logging test

--- test.py
+++ test.py
@@ -1,2 +1,24 @@
-import os
+import logging
+logging.basicConfig(
+	level=logging.CRITICAL,
+	format="{asctime} {levelname:<8} {message}",
+	style="{",
+	filename='%slog' % __file__[:-2],
+	filemode='w'
+)
+	
+
+logging.debug("This is a debug msg")
+logging.info("This is a info msg")
+logging.warning("This is a warning msg")
+logging.error("This is a error msg")
+logging.critical("This is a critical msg")
+
+a = 2
+b = 0
+try:
+	prod = a/b
+
+except Exception as e:
+	logging.critical("Exception occurred: ", exc_info=True)
 
